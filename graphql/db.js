0b8d6d773330dd03b11626416eabbd8f6b4afcd8
cemujax <cemujax@gmail.com>
2018-11-25 22:13:31 +0900

    creating mutation(addMovie, deleteMovie)

--- graphql/db.js
+++ graphql/db.js
@@ -1,21 +1,21 @@
 let movies = [
   {
-    id: "1",
+    id: 1,
     name: "Star Wars",
     score: 5
   },
   {
-    id: "2",
+    id: 2,
     name: "Avengers",
     score: 4
   },
   {
-    id: "3",
+    id: 3,
     name: "TransFormers",
     score: 4
   },
   {
-    id: "4",
+    id: 4,
     name: "Avatar",
     score: 3
   }
@@ -23,15 +23,25 @@ let movies = [
 
 export const getMovies = () => movies;
 
-export const getByMoive = id => {
-  const filterMovies = movies.filter(movie => movie.id === String(id));
+export const getById = id => {
+  const filterMovies = movies.filter(movie => movie.id === id);
   return filterMovies[0];
 };
 
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
+
 export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
   if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
+    movies = cleanedMovies;
     return true;
   } else {
     return false;
