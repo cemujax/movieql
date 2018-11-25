9e459f4b9dce575f096bcaff27aff3f5899bf493
cemujax <cemujax@gmail.com>
2018-11-25 19:44:54 +0900

    extending schema

---
+++ graphql/db.js
@@ -0,0 +1,31 @@
+export const people = [
+  {
+    id: "1",
+    name: "cemujax",
+    age: 11,
+    gender: "male"
+  },
+  {
+    id: "2",
+    name: "john",
+    age: 22,
+    gender: "male"
+  },
+  {
+    id: "3",
+    name: "james",
+    age: 33,
+    gender: "male"
+  },
+  {
+    id: "4",
+    name: "tom",
+    age: 44,
+    gender: "male"
+  }
+];
+
+export const getById = id => {
+  const filteredPeople = people.filter(person => id === people.id);
+  return filteredPeople[0];
+};
